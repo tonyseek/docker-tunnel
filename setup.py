acddb600aab332245136e97e877a9926509b3cfc
Jiangge Zhang <tonyseek@gmail.com>
2014-09-06 13:57:35 +0800

    Bump version: 0.1.0.dev → 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme:
 
 setup(
     name='docker-tunnel',
-    version='0.1.0.dev',
+    version='0.1.0',
     author='Jiangge Zhang',
     author_email='tonyseek@gmail.com',
     description='Using remote docker with SSH tunnel.',
